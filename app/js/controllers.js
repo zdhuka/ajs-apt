57c4543ae3c81b1fe7a871d5419694700d8ceee6
Zahid <zahid@ubuntu.(none)>
2013-06-27 11:21:29 +0530

    All code is commented now. Start the project from scratch

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,25 +1,25 @@
-'use strict';
-
-/* Controllers */
-
-function PhoneListCtrl($scope, Phone) {
-  $scope.phones = Phone.query();
-  $scope.tablets="samsung";
-  $scope.orderProp = 'age';
-}
-
-//PhoneListCtrl.$inject = ['$scope', 'Phone'];
-
-
-
-function PhoneDetailCtrl($scope, $routeParams, Phone) {
-  $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
-    $scope.mainImageUrl = phone.images[0];
-  });
-
-  $scope.setImage = function(imageUrl) {
-    $scope.mainImageUrl = imageUrl;
-  }
-}
-
-//PhoneDetailCtrl.$inject = ['$scope', '$routeParams', 'Phone'];
+//'use strict';
+//
+///* Controllers */
+//
+//function PhoneListCtrl($scope, Phone) {
+//  $scope.phones = Phone.query();
+//  $scope.tablets="samsung";
+//  $scope.orderProp = 'age';
+//}
+//
+////PhoneListCtrl.$inject = ['$scope', 'Phone'];
+//
+//
+//
+//function PhoneDetailCtrl($scope, $routeParams, Phone) {
+//  $scope.phone = Phone.get({phoneId: $routeParams.phoneId}, function(phone) {
+//    $scope.mainImageUrl = phone.images[0];
+//  });
+//
+//  $scope.setImage = function(imageUrl) {
+//    $scope.mainImageUrl = imageUrl;
+//  }
+//}
+//
+////PhoneDetailCtrl.$inject = ['$scope', '$routeParams', 'Phone'];
--- app/js/directives.js
+++ app/js/directives.js
@@ -1,3 +1,3 @@
-'use strict';
-
-/* Directives */
+//'use strict';
+//
+///* Directives */
